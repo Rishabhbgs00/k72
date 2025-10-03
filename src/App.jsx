9839ad5ceb8caf7a64b7057a1ff9064d668a572f
Rishabhbgs00 <113498408+Rishabhbgs00@users.noreply.github.com>
2025-10-03 22:25:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Agence from "./Pages/Agence";
 import Projects from "./Pages/Projects";
-import Navbarr from "./Components/agence/common/navbarr";
+import Navbarr from "./Components/agence/common/Navbarr";
 
-
-
- 
 function App() {
   return (
-    <div className='overflow-x-hidden'>
-      <BrowserRouter >
-    <Navbarr />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/agence" element={<Agence />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="overflow-x-hidden">
+      <BrowserRouter>
+        <Navbarr />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/agence" element={<Agence />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
