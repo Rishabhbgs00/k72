dcb433732391426f8ea9ca770362d253e8789209
Rishabhbgs00 <113498408+Rishabhbgs00@users.noreply.github.com>
2025-10-03 22:22:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Agence from "./Pages/Agence";
 import Projects from "./Pages/Projects";
-import Navbarr from "./Components/agence/common/navbarr";
 import Navbar from "./Components/agence/common/Navbarr";
 
 
